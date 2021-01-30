2370a5cccc0ca2876bf35fad647d877a9b79f860
Ömrüm Baki Temiz <omrumbakitemiz@icloud.com>
2021-01-30 14:04:19 +0300

    Initial commit

---
+++ apps/go-arena-web/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kafein-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'go-arena-web';
+}
